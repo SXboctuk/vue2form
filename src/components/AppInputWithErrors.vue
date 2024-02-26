dead5142d5bf1c44c7c358976668fa584a9c6272
Maxim <stargate.xboctuk@gmail.com@>
2024-02-26 17:29:15 +0300

    build component wrapper for errors input

---
+++ src/components/AppInputWithErrors.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <slot />
+        <template>
+            <app-input-errors :errors="errors" />
+        </template>
+    </div>
+</template>
+
+<script>
+import AppInputErrors from './AppInputErrors.vue';
+export default {
+    name: 'AppInputWithErrors',
+    components: {
+        AppInputErrors,
+    },
+    props: {
+        errors: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
