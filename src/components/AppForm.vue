8a2fe58803c03fbebc8b13842c7b66ff772df311
Maxim <stargate.xboctuk@gmail.com@>
2024-02-26 15:34:55 +0300

    build form component

---
+++ src/components/AppForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <form class="app-form" @submit.prevent="$emit('submit')">
+        <h2 v-if="title" class="app-form__title">{{ title }}</h2>
+        <slot />
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'AppForm',
+    props: {
+        title: {
+            type: String,
+            required: false,
+        },
+    },
+    emits: {
+        submit: {
+            required: false,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/variables.scss';
+.app-form {
+    max-width: 60rem;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 1.4rem;
+    &__title {
+        font-size: $titleSize;
+    }
+}
+</style>
